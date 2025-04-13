bcd8aa16559fdfa87f7247121cb0e0800fbf0830
Mehlsine <ismushir@gmail.com>
2025-04-13 11:57:05 +0400

    config

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,16 +24,16 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     // Component.PageTitle(),
-    // Component.MobileOnly(Component.Spacer()),
-    // Component.Flex({
-    //   components: [
-    //     {
-    //       Component: Component.Search(),
-    //       grow: true,
-    //     },
-    //     { Component: Component.Darkmode() },
-    //   ],
-    // }),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        // {
+        //   Component: Component.Search(),
+        //   grow: true,
+        // },
+        { Component: Component.Darkmode() },
+      ],
+    }),
     // Component.Explorer(),
   ],
   right: [
